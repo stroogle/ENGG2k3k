abd9d58a3daa31f2cfb947b8c351cdd5d19ce52c
JamesPirrie <james.pirrie@students.mq.edu.au>
2023-09-26 10:26:18 +1000

    New testing file

--- testing.cpp
+++ testing.cpp
@@ -1,74 +1,27 @@
-// Here is the document I will start to write tests in.
-// #include <Arduino.h>
-#include <iostream>
-#include <stdlib.h>
+#include <Arduino.h>
 #include <AUnit.h>
 
-/*
-Arduino have a unit testing library, AUnit. We will utilise this for the unit testing of our code. 
-Below is sample code before I knew about the AUnit library. Below I used user defined assertEquals similar
-to JUnit testing. 
-*/
+// Include functions and classes to be tested here.
+#include "interface.h"
 
-void setup()
-{
-    // Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+test(getMarbleCountTest) {
+    int expectedCount = 10; // number of marbles we manually insert and expect to see as "detected". Subject to change.
+    CounterControl count = new CounterControl(0);  // a new counter for when we run this test
+
+    int actualCount = count.getCount();
+    assertEqual(expectedCount, actualCount);
 }
 
-// SAMPLE: Function to add two integers
-int add(int a, int b)
-{
-    return a + b;
-}
-
-/*
-NOTE: This testing code will be added to the end of the interface.cpp file.
-This will make it easier for function calls to be used for testing.
-*/
-
-// eg:
-void testLightOn(int currentLightVal)
-{
-    // Serial.print("testLightOn :");
-    if (currentLightVal > 0)
-    {
-        // Serial.print("PASSED");
+void setup() {
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // Wait for the serial port to connect.
     }
-    else
-        // Serial.print("FAILED");
+
+    // Initialize the test runner and run the tests.
+    TestRunner::run();
 }
 
-// Coded in assertEquals similar to JUnit testing. Can be adjusted for different dataTypes.
-bool assertEquals(int actual, int expected, const std::string &testName)
-{
-    if (actual == expected)
-    {
-        // std::cout << std::endl is to be changed so we can use Serial.println()
-        std::cout << "PASS - " << testName << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "FAIL - " << testName << " (Expected: " << expected << ", Actual: " << actual << ")" << std::endl;
-        return false;
-    }
-}
-
-int main()
-{
-    // Call all tests. Using a test framework would simplify this.
-
-    // Test case 1
-    int result1 = add(5, 3);
-    assertEquals(result1, 8, "Test Case 1");
-
-    // Test case 2
-    int result2 = add(-2, 7);
-    assertEquals(result2, 5, "Test Case 2");
-
-    // Test case 3
-    int result3 = add(0, 0);
-    assertEquals(result3, 0, "Test Case 3");
-
-    return 0;
+void loop() {
+    // Nothing to do in the loop for unit testing as we only want tests to run once.
 }
