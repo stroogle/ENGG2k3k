3f5fe707f91bbd9a76491a64855410577dd248ee
ibromr <omeroglu.ibrahim@gmail.com>
2023-10-10 11:49:16 +1100

    testMarbleCountDisplay()ibrahim

--- testing.cpp
+++ testing.cpp
@@ -106,6 +106,112 @@ test(getMarbleCountTest) {
     assertEqual(expectedCount, actualCount);
 }
 
+test(detectedSensorOnDisplay) {  //  checking if Sensor works and send counts into LCD
+
+    // Define a mock class for SensorControl
+    class MockSensorControl : public SensorControl {
+    private:
+        int detectedAmount = 5;
+
+    public:
+        bool detected() override {
+            // Spoof the detected() function to always return true during testing
+            if (detectedAmount > 0) {
+                detectedAmount--;
+                return true
+            } else {
+                return false;
+            }
+        }
+    };
+
+    MockSensorControl sensor = new MockSensorControl(); // Assume sersor detected 5 times 
+    
+    CounterControl count = new CounterControl(0);  // a new counter for when we run this test
+
+
+    for(int i = 0; i <10; i++) {
+        // sensor is detected only 5 of the times.
+        if(sensor.detected()) {
+            count.incrementCount();   
+        }
+    }
+
+    assertEqual(count.getCount, 5);     //Assume this pass
+}   
+   
+
+
+test(mockMarbleCountDisplayPass){   // Mock sensor and display shows the same value so the function works
+    
+    // Define a mock class for SensorControl
+    class MockSensorControl : public SensorControl {
+    private:
+        int detectedAmount = 10;
+
+    public:
+        bool detected() override {
+            // Spoof the detected() function to always return true during testing
+            if (detectedAmount > 0) {
+                detectedAmount--;
+                return true
+            } else {
+                return false;
+            }
+        }
+    };
+
+    MockSensorControl sensor = new MockSensorControl(); // Getting true 
+    
+    CounterControl count = new CounterControl(10);  // Assume CounterControl works  
+
+
+    for(int i = 0; i <11; i++) {
+        // sensor is detected only 10 of the times.
+        if(sensor.detected()) {
+            count.incrementCount();  // As CounterControl, count will increase
+        }
+    }
+
+    assertEqual(display.showCount(counter.getCount()), 10);  // This test should pass
+}
+
+test(mockMarbleCountDisplayFail){ // Mock sensor and display shows different value 
+                                 //so the function does not work.
+    
+    // Define a mock class for SensorControl
+    class MockSensorControl : public SensorControl {
+    private:
+        int detectedAmount = 10;
+
+    public:
+        bool detected() override {
+            // Spoof the detected() function to always return true during testing
+            if (detectedAmt > 0) {
+                detectedAmt--;
+                return true
+            } else {
+                return false;
+            }
+        }
+    };
+
+    MockSensorControl sensor = new MockSensorControl();  // Getting true 
+    
+    CounterControl count = new CounterControl(8);  // Assume CounterControl does not work
+
+
+    for(int i = 0; i <11; i++) {
+        // sensor is detected only 10 of the times.
+        if(sensor.detected()) {
+            count.incrementCount();     // As CounterControl, count will increase til only 8
+        }
+    }
+
+    assertEqual(display.showCount(counter.getCount()), 10);  // This test should fail 
+}
+
+
 // SPEAKER CONTROL: THOMAS
 
 
