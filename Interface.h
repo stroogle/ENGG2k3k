68a4fbaefdd0602e34c62b200b5744a584ba74c8
Colm Moore <57727110+stroogle@users.noreply.github.com>
2023-10-24 10:32:05 +1100

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -95,132 +95,20 @@ class SensorControl {
 class MotorControl {
     // Assignee: COLM
     private:
-        Servo motor;
-        SensorControl sensor;
-        enum MotorState {Rotating, Stopped};
-        MotorState state;
-        unsigned long stoppedTimeStamp;
-        unsigned long rotatingTimeStamp;
-        int STOPPED_TIME_MS = 1300;
-        int ROTATE_TIME_MS = 5000;
-        int STOPPED_SPEED = 90;
-        int ROTATE_SPEED = -180;
-        int DETECTED_THRESHOLD_MS = 20000;
-
-        /**
-         * @brief Set the Speed object
-         * 
-         * @param speed The speed of the continuous motor
-         */
-        void setSpeed(int speed) {
-            motor.write(speed);
-        }
-
-        /**
-         * @brief Get the Speed object
-         * 
-         * @return int the speed of the continuous motor
-         */
-        int getSpeed() {
-            return motor.read();
-        }
-
-        /**
-         * @brief Used to check if the motor was last stopped timeMs ago.
-         * 
-         * @param timeMs - The amount of time in MS to check the motor has stopped for
-         * @return true - When it has been atleast timeMs since stoppedTimeStamp was set 
-         * @return false - Otherwise
-         */
-        bool hasStoppedFor(int timeMs) {
-            return (stoppedTimeStamp + timeMs) < millis();
-        }
-
-        /**
-         * @brief Used to check if the motor was last rotating timeMs ago
-         * 
-         * @param timeMs - The amount of time in MS to check the motor has been rotating for
-         * @return true - When the motor has been atleast timeMS since rotatingTimeStamp was set.
-         * @return false - Otherwise
-         */
-        bool hasRotatedFor(int timeMs) {
-            return (rotatingTimeStamp + timeMs) < millis();
-        }
-
-        /**
-         * @brief Stops the motor from turning.
-         * 
-         */
-        void stop() {
-            stoppedTimeStamp = millis();
-            state = Stopped;
-            setSpeed(STOPPED_SPEED);
-        }
-
-        /**
-         * @brief Starts the motor rotating
-         * 
-         */
-        void rotate() {
-            rotatingTimeStamp = millis();
-            state = Rotating;
-            setSpeed(ROTATE_SPEED);
-        }
-
+      Servo motor;
     public:
+      MotorControl(int motorPin){
+        motor.attach(motorPin);
+      }
 
-        MotorControl() {
-          motor.attach(1);
-          sensor = SensorControl();
-          stop();
+      void run(){
+        unsigned long time = (millis() / 1000) % 5;
+        if(time == 1) {
+          motor.write(90);
+        } else {
+          motor.write(180)
         }
-
-        /**
-         * @brief Construct a new Motor Control object
-         * 
-         * @param motorPin The ping to attach the motor too.
-         */
-        MotorControl(int motorPin, SensorControl s) {
-            pinMode(motorPin, INPUT_PULLUP);
-            motor.attach(motorPin);
-            sensor = s;
-            stop();
-        }
-
-        /**
-         * @brief Runs the motor stopping mechanism.
-         * 
-         */
-        void run() {
-            sensor.detected();
-            if(state == Stopped && hasStoppedFor(STOPPED_TIME_MS))
-            {
-                rotate();
-            }
-            else if (state == Rotating && hasRotatedFor(ROTATE_TIME_MS))
-            {
-                stop();
-            }
-        }
-
-        // LEGACY INTERFACE DESIGN, HERE INCASE WE DON'T GET CONTINOUS MOTOR.
-        /**
-         * @brief Rotates the motor the specified number of degrees.
-         * 
-         * @param deg 
-         */
-        void rotate(int deg) {
-            int currentRotation = getRotation();
-            motor.write(currentRotation + deg);
-        }
-
-        /**
-         * @brief Gets the servo's rotation
-         * 
-         */
-        int getRotation() {
-            return motor.read();
-        }
+      }
 };
 
 class LightingControl {
