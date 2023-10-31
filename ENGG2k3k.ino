2de27d5d758e95f1a6fc5159cfa35e05021191f0
alexeyk21 <47731644+alexeyk21@users.noreply.github.com>
2023-10-31 12:13:14 +1100

    Update ENGG2k3k.ino

--- ENGG2k3k.ino
+++ ENGG2k3k.ino
@@ -13,19 +13,24 @@
 
  Servo motor;
 LightingControl light;
-
-// const int LED_PIN = 8; // Pin to which the LED strip is connected
-// const int NUMBER_LEDS = 40; // Number of LEDs
-// int brightness = 255;
-// bool increase = false;
-// Adafruit_NeoPixel strip; // Adafruit_NeoPixel object
+SpeakerControl speaker;
+SensorControl s1;
+SensorControl s2;
+CounterControl counter;
+DisplayControl display;
+MarbleCountDisplay marbleDisplay;
 
 void setup() {
 
   Serial.begin(9600);
+  display.displaySetup();
 
-  // strip = Adafruit_NeoPixel(NUMBER_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-  // strip.begin();
+  s1 = SensorControl(2);
+  speaker = SpeakerControl(3, s1);
+
+  s2 = SensorControl(2);
+  counter = CounterControl(0);
+  marbleDisplay = MarbleCountDisplay(display, s2, counter);
 
   motor.attach(7);
 
@@ -45,23 +50,8 @@ void loop() {
       Serial.println("Rotating!");
   }
 
-  // if (brightness < 255 && increase == true) {
-  //     brightness+=5;
-  //     if (brightness == 255) {
-  //         increase = false;
-  //     }
-  // }
-  // if (brightness >= 0 && increase == false) {
-  //     brightness-=5;
-  //     if (brightness == 0) {
-  //         increase = true;
-  //     }
-  // }
-  // for (int i = 0; i < NUMBER_LEDS; i++) {
-  //     strip.setPixelColor(i, strip.Color(0, brightness, 0)); // Set color using Adafruit_NeoPixel's Color function
-  //     //strip.show();
-  // }
+  speaker.run();
 
-  // strip.show();
+  marbleDisplay.run();
     
 }
