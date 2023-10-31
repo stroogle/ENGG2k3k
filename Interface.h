fa02e8944f00036b2aa654554bbffd702a43ea0f
alexeyk21 <47731644+alexeyk21@users.noreply.github.com>
2023-10-31 12:12:51 +1100

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -61,6 +61,7 @@ class SensorControl {
          */
         bool detected() {
             bool sensorBlocked = lightIsBlocked();
+            // Serial.println("Speaker");
 
             if (sensorBlocked && state == Triggered)
             {
@@ -262,227 +263,213 @@ public:
     }
 };
 
-// class DisplayControl {
-//         public:
-//           static DFRobot_LedDisplayModule LED;
+class DisplayControl {
+        public:
+        LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);
 
-//         public:
-//           DisplayControl() {
-//               Serial.begin(115200);
-//               /*
-//               * Wait for the chip to be initialized completely, and then exit.
-//               * Select several bits for initialization, e8Bit for 8 bits and e4Bit for 4 bits.
-//               */
-//               while(LED.begin(LED.e8Bit) != 0)
-//               {
-//                   Serial.println("Failed to initialize the chip , please confirm the chip connection!");
-//                   delay(1000);
-//               }
-//               /*
-//               * Set the display area 
-//               * Please resend the display value if the display area is changed
-//               */
-//               LED.setDisplayArea(0,1,2,3,4,5,6,7); //need to test how setting of the setDisplayArea work
-//               LED.print("0","0","0","0","0","0","0","0");
+      DisplayControl() {
+
+    // Assignee: Andrei Ziganshin
+        /**
+         * @brief Displays the number provided onto the LED display 2x16 (bottom row). 
+         *  
+         *        Call showCount(number_of_marbles);
+         * 
+         * @param number
+         *
+         * put displaySetup(); and i2C_display_scan(); at the start of the main setup function
+         */
+       //  pinMode(A5, OUTPUT); //SCL pin **pending setup of the pin
+       //  pinnMode(A4, OUTPUT); //SDA pin **pending setup of the pin
+      }
+        char hex(int value) {
+        return "0123456789ABCDEF"[value & 0x0f]; 
+        }
+        void showCount(uint16_t val) {
+            lcd.setCursor(15,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+            
+            lcd.setCursor(14,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(13,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(12,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(11,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(10,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(9,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(8,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(7,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(6,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(5,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(4,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(3,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(2,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(1,1);
+            lcd.print(hex(val%10));
+            val = val/10;
+
+            lcd.setCursor(0,1);
+            lcd.print(hex(val%10));
+        }
+        void displaySetup(){
+            
+            lcd.init();                      // initialize the lcd 
+            // Print a message to the LCD.
+            lcd.backlight();
+            lcd.setCursor(0,0); // (1st number indicate the row "0" - top , "1" - bottom , 2nd- position starts from 0!)
+            lcd.print("T1 Marble Count:"); //top row message
+            
+        }
+};
+
+class CounterControl {
+    // Assignee: Ibrahim
+    private:
+        int counter;
+
+    public:
+        CounterControl() {
+            counter = 1;
+        }
+
+        CounterControl(int number) {
+            counter = number;
+        }
+        /**
+         * @brief Get the Count
+         * 
+         * @return int - The current count
+         */
+        int getCount() {
+            return counter;  // Return counter
+        }
+
+        /**
+         * @brief Set the Count object
+         * 
+         * @return int - The previous value of the counter
+         */
+        int setCount(int number) {
+
+            if(number > 99999999){
+                number = 0;
+            }
+            
+            int previousValue = counter; // Store the previous value
+            counter = number;           // Update the counter
+            return previousValue;       // Return the previous value
+        }
+
+        /**
+         * @brief Increment the Count of the object
+         * 
+         * @return int - The previous value of the counter
+         */
+        int incrementCount() {
+            return setCount(counter + 1); // Take stored value from setCount and incrementing by 1 and return it
+        }
+};
+
+class MarbleCountDisplay {
+    // Assignee: Thomas
+    private:
+        DisplayControl display;
+        SensorControl sensor;
+        CounterControl counter;
+
+    public:
+        MarbleCountDisplay() {
+          display = DisplayControl();
+          sensor = SensorControl();
+          counter = CounterControl();
+        }
+
+        MarbleCountDisplay(DisplayControl d, SensorControl s, CounterControl c) {
+          display = d;
+          sensor = s;
+          counter = c;
+        }
+
+        /**
+         * @brief Begins the Marble Counter Component
+         * 
+         */
+        void run() {
+            if(sensor.detected()) {
+                counter.incrementCount();
+            }
+            display.showCount(counter.getCount());
+        }
+};
+
+class SpeakerControl {
+
+    private:
+        SensorControl sensor;
+        int TONE_DELAY_MS = 40;
+        unsigned long LAST_TONE_PLAY;
+        int SPEAKER_PIN;
+
+    public:
+        SpeakerControl() {
+            sensor = SensorControl();
+            SPEAKER_PIN = 3;
+            LAST_TONE_PLAY = millis() + TONE_DELAY_MS;
+        }
+        SpeakerControl(int speakerPin, SensorControl s) {
+            sensor = s;
+            SPEAKER_PIN = speakerPin;
+            LAST_TONE_PLAY = millis() + TONE_DELAY_MS;
+        }
+
+        void run() {
+          
+            if(LAST_TONE_PLAY + TONE_DELAY_MS > millis()) {
+              noTone(SPEAKER_PIN);
+            } else if(sensor.detected()) {
               
-//           }
-
-//       class DisplayControl {
-//     // Assignee: Andrei Ziganshin
-//         /**
-//          * @brief Displays the number provided onto the LED display 2x16 (bottom row). 
-//          *  
-//          *        Call showCount(number_of_marbles);
-//          * 
-//          * @param number
-//          *
-//          * put displaySetup(); and i2C_display_scan(); at the start of the main setup function
-//          */
-//          pinMode(A1, OUTPUT); //SCL pin **pending setup of the pin
-//          pinnMode(A2, OUTPUT); //SDA pin **pending setup of the pin
-//         volatile uint8_t LCD_Addr = 0x27; //I2C Address of our display
-//         char hex(int value) {
-//         return "0123456789ABCDEF"[value & 0x0f]; 
-//         }
-//         void showCount(uint16_t val) {
-//             lcd.setCursor(0x0F);
-//             lcd.print(hex(val%10));
-//             val = val/10;
-            
-//             lcd.setCursor(0x0E);
-//             lcd.print(hex(val%10));
-//             val = val/10;
-
-//             lcd.setCursor(0x0D);
-//             lcd.print(hex(val%10));
-//             val = val/10;
-
-//             lcd.setCursor(0x0C);
-//             lcd.print(hex(val%10));
-//             val = val/10;
-
-//             lcd.setCursor(0x0B);
-//             lcd.print(hex(val%10));
-//         }
-//         void displaySetup(){
-
-//             LiquidCrystal_I2C lcd(LCD_Addr,20,4); //0x3F
-//             lcd.init();                      // initialize the lcd
-//             lcd.backlight(); 
-//             while (!Serial); //  wait for serial monitor
-//             Serial.println("\nI2C Scanner");
-//             Serial.begin(9600);
-//             lcd.setCursor(15,0); // (1st number indicate the row "0" - top , "1" - bottom , 2nd- position starts from 0!)
-//             lcd.print("T1 Box 2023"); //top row message
-//             lcd.setCursor(15,1);
-//             lcd.print("0000000000000000");
-//         }
-//         void i2C_display_scan(){
-//             byte error, address;
-//             int nDevices;
-            
-//             Serial.println("Scanning...");
-            
-//             nDevices = 0;
-//             for(address = 1; address < 127; address++ )
-//             {
-//                 // The i2c_scanner uses the return value of
-//                 // the Write.endTransmisstion to see if
-//                 // a device did acknowledge to the address.
-//                 Wire.beginTransmission(address);
-//                 error = Wire.endTransmission();
-            
-//                 if (error == 0)
-//                 {
-//                 Serial.print("I2C device found at address 0x");
-//                 if (address<16)
-//                     Serial.print("0");
-//                 Serial.print(address,HEX);
-//                 Serial.println("  !");
-            
-//                 nDevices++;
-//                 }
-//                 else if (error==4)
-//                 {
-//                 Serial.print("Unknown error at address 0x");
-//                 if (address<16)
-//                     Serial.print("0");
-//                 Serial.println(address,HEX);
-//                 }    
-//             }
-//             if (nDevices == 0)
-//                 Serial.println("No I2C devices found\n");
-//             else
-//                 Serial.println("done\n");
-            
-//             delay(5000);           // wait 5 seconds for next scan
-
-//         }
-// };
-
-// class CounterControl {
-//     // Assignee: Ibrahim
-//     private:
-//         int counter;
-
-//     public:
-//         CounterControl() {
-//             counter = 1;
-//         }
-
-//         CounterControl(int number) {
-//             counter = number;
-//         }
-//         /**
-//          * @brief Get the Count
-//          * 
-//          * @return int - The current count
-//          */
-//         int getCount() {
-//             return counter;  // Return counter
-//         }
-
-//         /**
-//          * @brief Set the Count object
-//          * 
-//          * @return int - The previous value of the counter
-//          */
-//         int setCount(int number) {
-
-//             if(number > 99999999){
-//                 number = 0;
-//             }
-            
-//             int previousValue = counter; // Store the previous value
-//             counter = number;           // Update the counter
-//             return previousValue;       // Return the previous value
-//         }
-
-//         /**
-//          * @brief Increment the Count of the object
-//          * 
-//          * @return int - The previous value of the counter
-//          */
-//         int incrementCount() {
-//             return setCount(counter + 1); // Take stored value from setCount and incrementing by 1 and return it
-//         }
-// };
-
-// class MarbleCountDisplay {
-//     // Assignee: Thomas
-//     private:
-//         DisplayControl display;
-//         SensorControl sensor;
-//         CounterControl counter;
-
-//     public:
-//         MarbleCountDisplay() {
-//           display = DisplayControl();
-//           sensor = SensorControl();
-//           counter = CounterControl();
-//         }
-
-//         MarbleCountDisplay(DisplayControl d, SensorControl s, CounterControl c) {
-//           display = d;
-//           sensor = s;
-//           counter = c;
-//         }
-
-//         /**
-//          * @brief Begins the Marble Counter Component
-//          * 
-//          */
-//         void run() {
-//             if(sensor.detected()) {
-//                 counter.incrementCount();
-//             }
-//             display.showCount(counter.getCount());
-//         }
-// };
-
-// class SpeakerControl {
-
-//     private:
-//         SensorControl sensor;
-//         int TONE_DELAY_MS = 40;
-//         unsigned long LAST_TONE_PLAY;
-//         int SPEAKER_PIN;
-
-//     public:
-//         SpeakerControl(int speakerPin, SensorControl s) {
-//             sensor = s;
-//             SPEAKER_PIN = speakerPin;
-//             LAST_TONE_PLAY = millis() + TONE_DELAY_MS;
-//         }
-
-//         void run() {
-//             if(LAST_TONE_PLAY + TONE_DELAY_MS < millis()) {
-//               noTone(SPEAKER_PIN);
-//             } else if(sensor.detected()) {
-//               tone(SPEAKER_PIN, 1047, 8);
-//               LAST_TONE_PLAY = millis();
-//             } 
-//         }
-// };
+              tone(SPEAKER_PIN, 1047, 8);
+              LAST_TONE_PLAY = millis();
+            } 
+        }
+};
 
 #endif
